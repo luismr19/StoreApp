3c333a7abde6f145dea9a29209026a235c64bc4f
danielGz <dan.gz.are@gmail.com>
2018-04-19 09:59:07 -0500

    Allowed for acentos to name and last name

--- src/main/java/com/pier/business/validation/FullNameValidator.java
+++ src/main/java/com/pier/business/validation/FullNameValidator.java
@@ -3,12 +3,12 @@ package com.pier.business.validation;
 public class FullNameValidator {
 	
 	public static boolean validateFirstName( String firstName )	   {
-	      return firstName.matches( "[A-Z][a-zA-Z]*" );
+	      return firstName.matches( "[A-ZáéíóúÁÉÍÓÚ][a-zA-ZáéíóúÁÉÍÓÚ]*" );
 	   } 
 	   
 	   public static boolean validateLastName( String lastName )
 	   {
-	      return lastName.matches( "[a-zA-Z]+([ '-][a-zA-Z]+)*" );
+	      return lastName.matches( "[a-zA-ZáéíóúÁÉÍÓÚ]+([ '-][a-zA-ZáéíóúÁÉÍÓÚ]+)*" );
 	   } // end method validateLastName
 
 }
